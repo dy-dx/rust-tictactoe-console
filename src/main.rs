82e4bdbf08e9cc102573df78eea497432918e25f
Chris Kim <christopherjkim@gmail.com>
2016-05-27 01:33:37 -0400

    draw the board

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use std::io;
 
+fn draw(board: &[[&str; 3]]) {
+    for i in 0..board.len() {
+        println!("-------------");
+        println!("{}", display_row(&board[i]));
+    }
+    println!("-------------");
+}
+
+fn display_cell(val: &str) -> &str {
+    if val == "" { " " } else { val }
+}
+
+fn display_row(row: &[&str]) -> String {
+    format!("| {} |", row.iter().map(|&x| display_cell(x)).collect::<Vec<&str>>().join(" | "))
+}
+
 fn main() {
 
-    let board: [[char; 3]; 3] = [[' '; 3]; 3];
+    let board: [[&str; 3]; 3] = [[""; 3]; 3];
+    draw(&board);
 
     println!("Please enter a move:");
 
